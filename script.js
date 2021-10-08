df3adc330069704ae8a022c354ff9603b53474a9
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-10-08 13:47:53 -0500

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+Project 2: Build a background color switcher.
+
+Requirements:
+-Create a Github repository for this project. 
+-Create an HTML select dropdown with at least 5 color options in it
+-use the "onchange" attribute so that every time a user selects a color using the dropdown, 
+it calls your JavaScript function that changes the background color of the BODY to the color selected in the dropdown
+
+
+
+*/
+
+
+function changeBackground () {
+    const select = document.getElementById("colors");
+    const color = select.options[select.selectedIndex].value;
+    document.body.style.background = color;
+    
+    
+
+    
+}
